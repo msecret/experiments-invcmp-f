a65315c9e1e9007d3208eeedbefd398ef9b57b04
Marco Secreto <msegreto@miceover.com>
2014-05-28 22:36:58 -0700

    Set up SCSS build compilation
    Fixes #35
    
    This was changes to less due to less being able to import the pure .css files
    easily.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 'use strict';
+var path = require('path');
 
 var gulp = require('gulp');
 var refresh = require('gulp-livereload');
 var livereload = require('tiny-lr');
 var server = livereload();
+var less = require('gulp-less');
 
 gulp.task('livereload-server', function () {
   server.listen(35729, function (err) {
@@ -15,6 +17,14 @@ gulp.task('css', function () {
   gulp.src('app/**/*.css').pipe(refresh(server));
 });
 
+gulp.task('less', function () {
+  gulp.src('./app/css/src/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./app/css/compiled'));
+});
+
 gulp.task('js', function () {
   gulp.src('app/**/*.js').pipe(refresh(server));
 });
@@ -22,8 +32,9 @@ gulp.task('js', function () {
 gulp.task('default', function () {
   gulp.run('livereload-server');
 
-  gulp.watch('app/**/*.css', function (event) {
+  gulp.watch('app/css/src/**/*.less', function (event) {
     gulp.run('css');
+    gulp.run('less');
   });
 
   gulp.watch('app/**/*.js', function (event) {
